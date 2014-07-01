c77f8b62b96d2984c6e0167a228166787e963e4f
tarnefys <tarnefys@gmail.com>
2014-07-01 17:13:30 +0200

    Create the Task table and insert values

--- install/create_sql.py
+++ install/create_sql.py
@@ -4,9 +4,24 @@
 import MySQLdb as mdb
 import sys
 import config
+import os 
+import json
+
+
+def open_json(filename):
+    try:
+        with open(filename) as f:
+            return json.load(f)
+    except:
+        sys.stderr.write("ERROR: Could not open file %s in dir %s" % (filename, dirpath))
+        exit(1)
+
+
+
+
 
 try:
-    con = mdb.connect(config.MYSQL['HOST'], config.MYSQL['USER'], config.MYSQL['PSWD'], config.MYSQL['DATABASE'])
+    con = mdb.connect(config.MYSQL['HOST'], config.MYSQL['USER'], config.MYSQL['PSWD'], config.MYSQL['DATABASE'], charset='utf8')
     cur = con.cursor()
 
     #cur.execute("SELECT VERSION()")
@@ -14,7 +29,28 @@ try:
     #ver = cur.fetchone()
     #print "Database version : %s " % ver
 
-    cur.execute("CREATE TABLE task (id int(10), PRIMARY KEY(id))");
+    
+    cur.execute("SET NAMES utf8;")
+    cur.execute("DROP TABLE task ")
+    cur.execute("CREATE TABLE task (id int(10) auto_increment, file_name varchar(250), format varchar(50), id_document int(10), lang varchar(50), line_nb int(10),path varchar(250),  PRIMARY KEY(id)) DEFAULT CHARSET=utf8");
+
+    res = cur.fetchone();
+
+    tasks = open_json("rep.json")
+    for task in tasks:
+
+        try:
+            query = "INSERT INTO task (file_name, format, id_document, lang, line_nb, path) VALUES (\"%s\", \"%s\", %s, \"%s\", %s, \"%s\")" \
+                % (task['file_name'], task['format'], task['id'], task['lang'], task['line_nb'], task['path'])
+            print query
+        
+            cur.execute(query);
+        except:
+            with open("error.txt", 'a') as myFile:
+                myFile.write("%s\n"%task)
+                
+
+
 
 
 
